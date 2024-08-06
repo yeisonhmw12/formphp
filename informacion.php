20e1eb0d7ea29fa287703735367db046a012d482
yeisonhmw12 <Salgadoyeison920@gmail.com>
2024-08-06 14:54:16 -0500

    Informacion.php

---
+++ informacion.php
@@ -0,0 +1,10 @@
+<?php
+$Identificacion= $_POST ['Identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado son: "NomApellidos;
+echo "El cago del empleado es: ".Cargo;
+echo "El salario del empeado es:  ".Salario;
+?>
